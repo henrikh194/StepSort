c44434d8482cfd7083574de8b9f3322b4771685a
henrikh194 <153110069+henrikh194@users.noreply.github.com>
2024-10-17 15:24:02 +0200

    Initial Upload of file

---
+++ stepsort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+# include <string.h>
+//methods for sorting come here :>
+
+
+int main(){
+    int numbers[] = {0,1,2,3,4,5,6,7,8,9};
+    char command[201];//command String is 200 chars wide, just in case :)
+    bool isRunning = true;
+    while(isRunning){
+        printf("You will search!\n");
+        scanf("%c", &command);
+        //here we compare the char arrays with strcmp(str1,str2)
+        //should return 0 if str1 = str2 :)
+    }
+    return 0;
+}
