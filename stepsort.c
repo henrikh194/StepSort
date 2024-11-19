899ad176c36830136f95f7b8506ea67c6850829e
Henrik H. <henrik.hinterland@gmail.com>
2024-11-19 22:57:29 +0100

    Changed Structure, added functionality, got the program to work

--- stepsort.c
+++ stepsort.c
@@ -1,31 +1,63 @@
 #include <stdio.h>
 # include <string.h>
 #include <stdbool.h>
-//methods for sorting come here :>
-//list of sorting algorithms to be implemented:
-//bubble sort, selection sort, stalin sort, sleep sort 
+
+void arrayPrint(int array[], int length){
+    for (int i = 0; i < length; i++){
+            printf("[%d]", array[i]); 
+        }
+}
+
+void bubbleSortStep(int array[], int length) {
+    for (int i = 0; i < length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            int memory = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = memory;
+            break;
+        }
+    }
+}
+
+int stalinSort(int array[], int length) {
+    int modifiedSizeOfArray = 1; 
+
+    for (int i = 1; i < length; i++) {
+        if (array[i] >= array[modifiedSizeOfArray - 1]) {
+            array[modifiedSizeOfArray] = array[i];
+            modifiedSizeOfArray++;
+        }
+    }
+    return modifiedSizeOfArray;
+}
 
 
 int main(){
-    int numbers[] = {0,1,2,3,4,5,6,7,8,9};
-    char command[201];//command String is 200 chars wide, just in case :)
+    int numbers[] = {4,3,1,2,5,0,6,7,8,9};
+    char command[201];
+    int numbersLength = 10;
     bool isRunning = true;
-    while(isRunning == true){
-        printf("Welcome to Stepsort. :)\n");
-        printf("\tYou will have the array:\n\t");
-        for (int i = 0; i < 10; i++){
-            printf("[%d]", numbers[i]); 
-        }
-        printf("\n\twhich you can sort with various sorting algorithms. :D\n");
+    
+
+    printf("Welcome to Stepsort. :)\n");
+    printf("\tYou will have the array:\n\t");
+    arrayPrint(numbers, numbersLength);
+    printf("\n\twhich you can sort with various sorting algorithms. :D\n");
+
+    while(isRunning == true){   
+        printf("\n Enter your command:");
         scanf("%200s", command);
+
         if (strcmp(command,"Bubblesort") == 0){
-            //call bubblesort funtion <3
+            bubbleSortStep(numbers, numbersLength);
+            arrayPrint(numbers, numbersLength);
+        } else if (strcmp(command,"Stalinsort") == 0){
+            numbersLength = stalinSort(numbers, numbersLength);
+            arrayPrint(numbers, numbersLength);
         } else if (strcmp(command,"Q") == 0){
             isRunning = false;
+            printf("Thank you for using StepSort");
         } 
-        
-        //here we compare the char arrays with strcmp(str1,str2)
-        //should return 0 if str1 = str2 :)
     }
     return 0;
 }
